998098e85fdbb1f994b7685949826e829b216ec7
AHM-Moinuddin-Sami <moinuddinsami1205@gmail.com>
2023-05-19 16:46:29 +0600

    added firebase and authprovider

--- src/Components/AuthProvider/AuthProvider.jsx
+++ src/Components/AuthProvider/AuthProvider.jsx
@@ -1,9 +1,65 @@
+import { createContext, useEffect, useState } from "react";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import app from "../Firebase/firebase.config";
 
-const AuthProvider = () => {
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
+const googleProvider = new GoogleAuthProvider();
+
+const AuthProvider = ({children}) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const register = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const logIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    const googleLogin = () => {
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, loggedUser => {
+            setUser(loggedUser);
+            if (loggedUser != null) {
+                loggedUser.providerData.forEach(profile => {
+                    console.log(profile.photoURL);
+                });
+            }
+            setLoading(false);
+
+        })
+
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        googleLogin,
+        logIn,
+        logOut,
+        register
+    }
+
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
