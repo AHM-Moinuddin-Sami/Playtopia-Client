328f52421912c1a6a1ebf7f2b5b6af9ab467d382
AHM-Moinuddin-Sami <moinuddinsami1205@gmail.com>
2023-05-21 11:43:35 +0600

    added shop by category in homepage

--- src/Components/Main/ToyCategory/ToyCategory.jsx
+++ src/Components/Main/ToyCategory/ToyCategory.jsx
@@ -1,21 +1,79 @@
+import { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import ToyCategoryCard from './ToyCategoryCard/ToyCategoryCard';
 
 const ToyCategory = () => {
-    return (
-        <Tabs>
-            <TabList>
-                <Tab>Title 1</Tab>
-                <Tab>Title 2</Tab>
-            </TabList>
+    const [toys, setToys] = useState([]);
 
-            <TabPanel>
-                <h2>Any content 1</h2>
-            </TabPanel>
-            <TabPanel>
-                <h2>Any content 2</h2>
-            </TabPanel>
-        </Tabs>
+    useEffect(() => {
+        fetch('http://localhost:5000/toys')
+            .then(res => res.json())
+            .then(data => setToys(data));
+
+    }, []);
+
+    return (
+        <div className='my-5'>
+            <h3 className='text-center text-5xl mb-5 font-semibold'>Browse toys by Category</h3>
+            <Tabs>
+                <div className="text-center">
+                    <TabList >
+                        <Tab>Traditional Boardgames</Tab>
+                        <Tab>Tabletop Boardgames</Tab>
+                        <Tab>Puzzles</Tab>
+                    </TabList>
+                </div>
+                <TabPanel>
+                    <div className='grid md:grid-cols-3 gap-3 justify-items-center bg-white' style={{ backgroundImage: `url("https://i.pinimg.com/originals/3c/24/46/3c24462450c2a902bf7e18f3d9aada81.jpg")` }}>
+                        {
+                            toys.map(toy => {
+                                if (toy.subCategory === "traditional")
+                                    return <ToyCategoryCard key={toy._id}
+                                        name={toy.toyName}
+                                        photo={toy.photo}
+                                        price={toy.price}
+                                        rating={toy.rating}
+                                        id={toy._id}
+                                    ></ToyCategoryCard>
+                            })
+                        }
+                    </div>
+                </TabPanel>
+                <TabPanel>
+                    <div className='grid md:grid-cols-3 gap-3 justify-items-center bg-white' style={{ backgroundImage: `url("https://i.pinimg.com/originals/3c/24/46/3c24462450c2a902bf7e18f3d9aada81.jpg")` }}>
+                        {
+                            toys.map(toy => {
+                                if (toy.subCategory === "tabletop")
+                                    return <ToyCategoryCard key={toy._id}
+                                        name={toy.toyName}
+                                        photo={toy.photo}
+                                        price={toy.price}
+                                        rating={toy.rating}
+                                        id={toy._id}
+                                    ></ToyCategoryCard>
+                            })
+                        }
+                    </div>
+                </TabPanel>
+                <TabPanel>
+                    <div className='grid md:grid-cols-3 gap-3 justify-items-center bg-white' style={{ backgroundImage: `url("https://i.pinimg.com/originals/3c/24/46/3c24462450c2a902bf7e18f3d9aada81.jpg")` }}>
+                        {
+                            toys.map(toy => {
+                                if (toy.subCategory === "puzzle")
+                                    return <ToyCategoryCard key={toy._id}
+                                        name={toy.toyName}
+                                        photo={toy.photo}
+                                        price={toy.price}
+                                        rating={toy.rating}
+                                        id={toy._id}
+                                    ></ToyCategoryCard>
+                            })
+                        }
+                    </div>
+                </TabPanel>
+            </Tabs>
+        </div>
     );
 };
 
---
+++ src/Components/Main/ToyCategory/ToyCategoryCard/ToyCategoryCard.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const ToyCategoryCard = ({ photo, name, price, rating, id }) => {
+    return (
+        <div className="card w-96 bg-white my-3">
+            <figure><img className="h-72 w-full object-fit" src={photo} /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                <p>Price: {price} $ Rating: {rating}</p>
+                <div className="card-actions justify-end">
+                    <Link to={`/toys/${id}`} className="btn btn-primary">View Details</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ToyCategoryCard;
